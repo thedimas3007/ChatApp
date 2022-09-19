fd41d64b1841f0feb1ca48f7eb218c9b4b4f1950
thedimas3007 <dima.orischenko@gmail.com>
2022-09-19 22:31:17 +0300

    basic ui

--- Chat/ContentView.swift
+++ Chat/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var serverIP = "";
+    @State var server = false
+    @State var nickname = ""
+    @State var connected = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if !connected {
+            List {
+                Section {
+                    TextField("Server IP", text: $serverIP).disabled(server)
+                    TextField("Nickname", text: $nickname)
+                    Toggle("Host", isOn: $server).onChange(of: server) {newValue in
+                        serverIP = newValue ? "127.0.0.1" : ""
+                    }
+                    Button("Connect") {
+                        connected = true
+                    }
+                    
+                }
+            }
+        } else {
+            Text("Deez nuts")
+        }
     }
 }
 
